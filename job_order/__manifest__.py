e8c9d23ee3731479c08afeeba11c1afe91c397dc
anne-pg <anne@plonturgroup.com>
2026-03-06 09:58:35 +0800

    Update __manifest__.py

--- job_order/__manifest__.py
+++ job_order/__manifest__.py
@@ -1,12 +1,13 @@
 {
     'name': 'Purchase Order Job State',
     'version': '1.0',
-    'category': 'Inventory/Purchase',
     'summary': 'Adds Job state to Purchase Orders',
     'depends': ['purchase'],
     'data': [
         'views/purchase_order_views.xml',
     ],
     'installable': True,
+    'application': True,
     'license': 'LGPL-3',
+
 }
