df55e47a743043ba44037d12e22c5cf83aae6674
anne-pg <anne@plonturgroup.com>
2026-03-06 10:36:42 +0800

    Delete job_order directory

--- job_order/models/purchase_order.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-
-    state = fields.Selection(selection_add=[
-        ('job', 'Job Order'),
-    ], ondelete={'job': 'set default'})
