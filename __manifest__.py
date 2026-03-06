3e7cbc149f9d0c10212949d2667516cb3f9cfcb4
anne-pg <anne@plonturgroup.com>
2026-03-06 10:37:00 +0800

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Purchase Order Job State',
+    'version': '1.0',
+    'category': 'Inventory/Purchase',
+    'summary': 'Adds Job state to Purchase Orders',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order_views.xml',
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
